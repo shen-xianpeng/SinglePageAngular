a3c335e66d9ba7d79a3eff334d3f1420b3ebc66b
shen.xianpeng <shenxianpeng1991@gmail.com>
2014-05-03 10:03:49 +0800

    single page app with Angular

---
+++ page/urls.py
@@ -0,0 +1,14 @@
+﻿from django.conf.urls import patterns, include, url
+import os
+
+
+from .views import index
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'UpFile.views.home', name='home'),
+    url(r'^$', index),
+
+ 
+
+)
---
+++ page/views.py
@@ -0,0 +1,12 @@
+﻿from django.core.urlresolvers import reverse
+from django.shortcuts import render_to_response, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import RequestContext
+
+
+
+def index(request, templates="CustomerManagementApp.html"):
+    template_var={}
+    return render_to_response(templates, template_var, context_instance=RequestContext(request))
+
+
